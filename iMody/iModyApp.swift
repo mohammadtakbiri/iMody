fcb21248e53f2bbbc43bf52407ee5a048cadb2c2
Mohammad Takbiri <mohammad_t@flexin.io>
2025-04-13 21:17:07 +0330

    Initial Commit

---
+++ iMody/iModyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iModyApp.swift
+//  iMody
+//
+//  Created by Mohammad Takbiri on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct iModyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
